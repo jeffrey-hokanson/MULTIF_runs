87f7fe31562be2f2541241f458800fbef3d8879f
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2018-04-02 15:07:54 -0600

    New outputs

--- Param136/plot_sweep.py
+++ Param136/plot_sweep.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 
 from sequoia import *
 
-levels = [0,2,5]
+levels = [0,2,5,11]
 index = [0,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
 
 Ys = []
 for level in levels:
-	Ys.append(np.loadtxt('sweep1_level%d_v16.output' % level ))
+	Ys.append(np.loadtxt('sweep1_level%d_v17.output' % level ))
 
 
 for k in range(0,10):
