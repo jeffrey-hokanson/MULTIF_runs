3d3497090f083389b0af8275d5fd1bd1d74c8a3a
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2018-04-23 12:08:07 -0600

    Start of sweep3 on 3D RANS

--- Param136/plot_sweep3.py
+++ Param136/plot_sweep3.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import itertools
 from sequoia import *
 
-levels = [0,2,5]
+levels = [0,2,5,11]
 index = [0,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+
+kwargs = itertools.cycle( (
+	{'marker': '.', 'markersize': 4},
+	{'marker': '+', 'markersize': 4},
+	{'marker': 'x', 'markersize': 4},
+	{'marker': 'o', 'markersize': 4, 'mfc' : 'none'}))
+
 
 Ys = []
 for level in levels:
@@ -18,7 +25,7 @@ for k in range(0,50):
 	for ax, i in zip(axes.flatten(), index):
 		ax.set_title(qoi_names[i])
 		for Y, level in zip(Ys, levels):
-			ax.plot(Y[I,i], '.', label = level_names[level], markersize = 3 )
+			ax.plot(Y[I,i], linestyle = 'None', label = level_names[level], **kwargs.next())
 		if i == 3:
 			pass
 			#ax.set_ylim(bottom = 0, top = 60e3)
